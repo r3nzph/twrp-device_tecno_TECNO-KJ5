bd2fe0eecaa364c5766943aa40523b67e9901b64
r3nzph <abarracosorenznino@gmail.com>
2025-04-17 04:31:51 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/r3nzph/-twrp-device_transsion_mt6768-common.git -b twrp device/transsion/mt6768-common
+git clone https://github.com/r3nzph/twrp-device_transsion_mt6768-common.git -b twrp device/transsion/mt6768-common
