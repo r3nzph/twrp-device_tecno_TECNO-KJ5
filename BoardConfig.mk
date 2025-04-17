4533c795f2b48966d4f559caad4e3da4969ab68b
r3nzph <abarracosorenznino@gmail.com>
2025-04-17 18:32:09 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,9 +6,5 @@ include device/transsion/mt6768-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := KJ5
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_TECNO-KJ5
-TARGET_RECOVERY_DEVICE_MODULES := libinit_TECNO-KJ5
-
 # TWRP Configs
 TW_DEVICE_VERSION := KJ5_by_r3nzph
